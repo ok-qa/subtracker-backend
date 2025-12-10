cb0dbe49bc92537d5b0f2bd79ec3172e0562637a
ok-qa <olechka.kr@gmail.com>
2025-12-10 10:27:53 -0800

    save avatar to Cloudinary

--- src/constants/index.js
+++ src/constants/index.js
@@ -11,3 +11,10 @@ export const ONE_DAY = 24 * 60 * 60 * 1000;
 export const TEMP_UPLOAD_DIR = path.join(process.cwd(), "temp");
 export const UPLOAD_DIR = path.join(process.cwd(), "uploads");
 export const TEMPLATES_DIR = path.join(process.cwd(), "src", "templates");
+
+export const CLOUDINARY = {
+  CLOUD_NAME: "CLOUDINARY_CLOUD_NAME",
+  API_KEY: "CLOUDINARY_API_KEY",
+  API_SECRET: "CLOUDINARY_API_SECRET",
+  ENABLE_CLOUDINARY: "ENABLE_CLOUDINARY",
+};
