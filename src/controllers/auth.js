79345b918337e1b5fd284df894b56b983dc3866d
ok-qa <olechka.kr@gmail.com>
2026-01-12 12:00:12 -0800

    debug live with console

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -118,6 +118,8 @@ export const getGoogleOAuthUrlController = async (req, res) => {
 export const loginWithGoogleController = async (req, res) => {
   const session = await loginOrSignupWithGoogle(req.query.code);
   req.session.oauthAccessToken = session.accessToken;
+  console.log("loginWithGoogleController session.accessToken: ", session.accessToken);
+  console.log("loginWithGoogleController req.session: ", req.session);
   setupSession(res, session);
 
   const {state} = req.query;
@@ -130,6 +132,7 @@ export const loginWithGoogleController = async (req, res) => {
 };
 
 export const OAuthTokenController = async (req, res) => {
+  console.log("OAuthTokenController req.session: ", req.session);
   const session = req.session;
   if (!session || !session.oauthAccessToken) {
     return res.json({
