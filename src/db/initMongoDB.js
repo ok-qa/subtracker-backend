288db9a8918bae667d97a3ed83472e4d966294d8
ok-qa <olechka.kr@gmail.com>
2026-03-04 11:48:47 -0800

    added separate variable for database option

--- src/db/initMongoDB.js
+++ src/db/initMongoDB.js
@@ -3,18 +3,18 @@ import { env } from "../utils/env.js";
 
 const initMongoDB = async () => {
   try {
-    const isProd = String(env("IS_PROD")) === "true";
+    const isLiveDatabase = String(env("LIVE_DATABASE")) === "true";
 
     const user = env("MONGODB_USER");
     const pwd = env("MONGODB_PASSWORD");
     const url = env("MONGODB_URL");
-    const db = isProd ? env("MONGODB_DB_LIVE") : env("MONGODB_DB");
+    const db = isLiveDatabase ? env("MONGODB_DB_LIVE") : env("MONGODB_DB");
 
     await mongoose.connect(
       `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
     );
 
-    const dbType = isProd ? "LIVE_DB" : "DEV_DB";
+    const dbType = isLiveDatabase ? "LIVE_DB" : "DEV_DB";
 
     console.log(`Mongo connection successfully established on ${dbType}!`);
   } catch (error) {
